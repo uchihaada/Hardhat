1d79804d482fce81a71606ca3f811819470feab1
Abhijit Das <abhijitadadas@gmail.com>
2023-04-02 21:22:26 +0530

    final commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,16 @@
 /** @type import('hardhat/config').HardhatUserConfig */
-require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-waffle");
+
+const ALCHEMY_API_KEY="9d8EJfGWp05LCxfn693w1uRI1q2lGTuy";
+const SEPOLIA_PRIVATE_KEY="6f463f7a8b5b6eb3859d95435789358fcf81ece2a3f5aa8ae1c842e2fc77e3f9";
+// https://eth-sepolia.g.alchemy.com/v2/9d8EJfGWp05LCxfn693w1uRI1q2lGTuy
+
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY]
+    }
+  }
 };
